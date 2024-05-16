98ab2ec9135964036e3c2ce01bbf62d55df9418f
Rashmi Amarasekara <164611387+rash-004@users.noreply.github.com>
2024-05-16 19:49:30 +0530

    Update rash_004.js

--- rash_004.js
+++ rash_004.js
@@ -4,3 +4,18 @@ const background = document.querySelector(".background");
 hamburger_menu.addEventListener("click", () => {
     background.classList.toggle("active");
 })
+
+document.getElementById('btn').addEventListener('click', function() {
+    document.getElementById('about_me').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 0) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
