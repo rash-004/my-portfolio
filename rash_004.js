7c7225db261f868f13332f4917c449947728e39e
Rashmi Amarasekara <164611387+rash-004@users.noreply.github.com>
2024-05-23 22:47:36 +0530

    Update rash_004.js

--- rash_004.js
+++ rash_004.js
@@ -8,7 +8,8 @@ function redirectToDesktopVersion() {
     }
 }
 
-    redirectToDesktopVersion();
+redirectToDesktopVersion();
+
 
 const hamburger_menu = document.querySelector(".hamburger_menu");
 const background = document.querySelector(".background");
@@ -17,12 +18,7 @@ hamburger_menu.addEventListener("click", () => {
     background.classList.toggle("active");
 })
 
-document.getElementById('btn').addEventListener('click', function() {
-    document.getElementById('about_me').scrollIntoView({
-        behavior: 'smooth'
-    });
-});
-
+/*navigation -bar change color*/
 window.addEventListener('scroll', function() {
     const navbar = document.getElementById('navbar');
     if (window.scrollY > 0) {
@@ -32,8 +28,41 @@ window.addEventListener('scroll', function() {
     }
 });
 
+document.getElementById('home').addEventListener('click', function() {
+    document.getElementById('main').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById('btn').addEventListener('click', function() {
+    document.getElementById('about_me').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+
 document.getElementById('educ').addEventListener('click', function() {
     document.getElementById('education').scrollIntoView({
         behavior: 'smooth'
     });
 });
+
+
+document.getElementById('ex_act').addEventListener('click', function() {
+    document.getElementById('extra').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById('proj').addEventListener('click', function() {
+    document.getElementById('projects').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+document.getElementById('cont').addEventListener('click', function() {
+    document.getElementById('contact').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
